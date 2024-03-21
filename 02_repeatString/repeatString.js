cc2aca76834ace7ba2d922953371ce010680c3d0
Anand RAj <anandraj6783@gmail.com>
2024-03-21 10:29:12 +0530

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(string,num) {
+  let i=0;
+  let finalString='';
+  if(num<0)
+  {
+    finalString ='ERROR';
+  }
+    for(i===0;i<num;i++)
+    {
+      finalString+=string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
