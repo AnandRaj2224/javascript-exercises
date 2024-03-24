54be8346cf91a7e252514d656e4d9ccb89b20bac
Anand RAj <anandraj6783@gmail.com>
2024-03-24 20:31:55 +0530

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const degreeCelsius = (5/9)*(temperature-32);
+  return Number(degreeCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const degreeFahrenheit = (9/5 *temperature) + 32;
+  return Number(degreeFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
