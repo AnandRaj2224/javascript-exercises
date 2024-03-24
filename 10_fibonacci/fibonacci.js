1106b6d944883455ed356fe155b52d2dce5c2395
Anand RAj <anandraj6783@gmail.com>
2024-03-24 23:46:05 +0530

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+  if(position<0) return "OOPS";
+  let array =new Array();
+  array[0]=0;
+  array[1]=1;
+  array[2]=1;
+  for(let i = 3;i<=position;i++)
+  {
+    array[i]=array[i-1]+array[i-2];
+  }
+  return array[position];
 };
 
 // Do not edit below this line
