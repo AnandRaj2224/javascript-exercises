59fc4ad5363dfd3f265f4b2c90bf748530b8fd72
Anand RAj <anandraj6783@gmail.com>
2024-03-24 21:25:55 +0530

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(number1,number2) {
+  const sum =number1+number2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1,number2) {
+  const sum =number1-number2;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  let i =0;
+	if(array.length===0){
+   return 0;
+  }
+  else{
+  while(i<array.length){
+    sum+=array[i];
+    i++;
+  }
+  return sum;
+}
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  let i =0;
+  while(i<array.length){
+    product*=array[i];
+    i++;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	const answer = Math.pow(base,exponent);
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let sum=1;
+  if(number === 1 || number === 0)
+  {
+    return 1;
+  }
+  else{
+    for(let i =2;i<=number;i++)
+    {
+      sum*= i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
