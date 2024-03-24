75a6decfebb13c763bef71deb546baffdbe9c0a4
Anand RAj <anandraj6783@gmail.com>
2024-03-24 14:23:54 +0530

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+  let sum =0;
+  const check ='ERROR';
+  let startDigit;
+  let endDigit;
+  if(firstNumber>secondNumber)
+  {
+    startDigit=secondNumber;
+    endDigit=firstNumber;
+  }
+  else
+  {
+    startDigit=firstNumber;
+    endDigit=secondNumber;
+  }
+  if(startDigit < 0 || endDigit < 0 || typeof firstNumber !== 'number' || typeof secondNumber !== 'number' )
+  {
+     return check;
+  }
+  else{
+    for(let i=startDigit;i<=endDigit;i++)
+  {
+    sum+=i;
+  }
+  return sum;
+  }
+  
 };
 
 // Do not edit below this line
