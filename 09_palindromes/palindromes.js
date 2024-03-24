e1bce8f346cd5127dcb59bcef19d79dbdba62a74
Anand RAj <anandraj6783@gmail.com>
2024-03-24 23:09:54 +0530

    not able to understand fully exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let charsWithoutExclamation = str.split("").filter(char => char !== "!");
+  // Reverse the array of characters
+  let reversedChars = charsWithoutExclamation.reverse();
+  // Insert exclamation marks at their original positions
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "!") {
+      reversedChars.splice(i, 0, "!");
+    }
+  }
+  // Join the characters in the reversedChars array to form the reversed string
+   if((reversedChars.join("")).toUpperCase() === str.toUpperCase()) return true;
+   else return false;
 };
 
 // Do not edit below this line
